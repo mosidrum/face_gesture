3f03a5af348bad6a8b6c733ea3d2d5f29772aa65
mosidrum <mosiokanga@gmail.com>
2024-10-11 20:29:47 +0100

    add bootstrap and styling with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useLoadModels } from "./hooks/useLoadModels";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { VideoCanvas } from "./component";
-import { useLoadModels } from "./hooks/useLoadModels.ts";
 
 export const App = () => {
   const [initModel, setInitModel] = useState(false);
@@ -8,8 +9,18 @@ export const App = () => {
   useLoadModels(setInitModel);
 
   return (
-    <div>
-      <span>{!initModel ? "Initializing models..." : "Models ready!"}</span>
+    <div className="container text-center mt-5">
+      <h3>Face Detection App</h3>
+      <div className="mt-3">
+        <span
+          className={`badge ${initModel ? "bg-success" : "bg-warning"} p-2`}
+        >
+          {!initModel
+            ? " Wait, while Initializing models..."
+            : "Models are ready!"}
+        </span>
+      </div>
+
       {initModel && <VideoCanvas />}
     </div>
   );
--- src/component/videoCanva/videoCanva.tsx
+++ src/component/videoCanva/videoCanva.tsx
@@ -1,9 +1,9 @@
-import { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { startVideoStream } from "../../utils";
 import { useFaceDetection } from "../../hooks";
 import { videoHeight, videoWidth } from "../../types";
 
-export const VideoCanvas = () => {
+export const VideoCanvas: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
@@ -16,29 +16,35 @@ export const VideoCanvas = () => {
   useFaceDetection(videoRef, canvasRef);
 
   return (
-    <div
-      style={{ position: "relative", width: videoWidth, height: videoHeight }}
-    >
-      <div style={{ position: "relative" }}>
-        <video
-          ref={videoRef}
-          autoPlay
-          muted
-          height={videoHeight}
-          width={videoWidth}
-          style={{ position: "absolute", top: 0, left: 0 }}
-        />
-        <canvas
-          ref={canvasRef}
-          height={videoHeight}
-          width={videoWidth}
+    <div className="text-center mt-4">
+      <div className="d-flex justify-content-center align-items-center">
+        <div
           style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 1,
+            position: "relative",
+            width: videoWidth,
+            height: videoHeight,
           }}
-        />
+        >
+          <video
+            ref={videoRef}
+            autoPlay
+            muted
+            height={videoHeight}
+            width={videoWidth}
+            style={{ position: "absolute", top: 0, left: 0 }}
+          />
+          <canvas
+            ref={canvasRef}
+            height={videoHeight}
+            width={videoWidth}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: 1,
+            }}
+          />
+        </div>
       </div>
     </div>
   );
